59fa1f09a96fe33d937716c5fdbc3a0c3be085bd
Anas Muhsin <anashanoon2006@gmail.com>
2023-07-31 10:13:05 +0400

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Anas Muhsin on 31/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
